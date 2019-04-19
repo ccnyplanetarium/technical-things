3bae7b6e875befa218fb39a7743b2ffbf342e836
James Hedberg <jhedberg@ccny.cuny.edu>
2019-04-19 15:47:03 -0400

    customization lua script file

---
+++ customization.lua
@@ -0,0 +1,7 @@
+openspace.setPropertyValueSingle('RenderEngine.Background Exposure', 8.000000);
+openspace.setPropertyValueSingle("RenderEngine.HDRExposure", 0.210000)
+openspace.setPropertyValue('Scene.*Trail.Renderable.Enabled', false)
+openspace.setPropertyValueSingle("Scene.Mars.Renderable.Layers.HeightLayers.A_hrsm-mola.Enabled", true)
+openspace.setPropertyValueSingle("Scene.Mars.Renderable.Layers.HeightLayers.Mola_Utah.Enabled", false)
+openspace.setPropertyValueSingle("Scene.Mars.Renderable.Layers.ColorLayers.MOC_WA_Color_Utah.Settings.Gamma", 1.66)
+openspace.setPropertyValueSingle("Scene.Stars.Renderable.ScaleFactor", 2.000000)
